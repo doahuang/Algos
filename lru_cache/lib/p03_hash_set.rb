72b580f8c9781638ba57247efb569ac7e3cefdd6
doahuang <doahuang@gmail.com>
2018-08-15 14:27:46 -0700

    Passed hash set

--- lru_cache/lib/p03_hash_set.rb
+++ lru_cache/lib/p03_hash_set.rb
@@ -7,18 +7,25 @@ class HashSet
   end
 
   def insert(key)
+    self[key.hash] << key
+    @count += 1
+    resize! if count > num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self[key.hash].delete(key)
   end
 
   private
+  attr_reader :store
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +33,11 @@ class HashSet
   end
 
   def resize!
+    old_store = store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    old_store.flatten.each do |key|
+      insert(key)
+    end
   end
 end
