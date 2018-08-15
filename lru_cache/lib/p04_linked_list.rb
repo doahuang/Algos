ed342336f6a4f5588613766c9c802976edcfe863
doahuang <doahuang@gmail.com>
2018-08-15 15:12:34 -0700

    Passed linked list

--- lru_cache/lib/p04_linked_list.rb
+++ lru_cache/lib/p04_linked_list.rb
@@ -15,11 +15,20 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
+    @next, @prev = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    head.next = tail
+    tail.prev = head
   end
 
   def [](i)
@@ -28,34 +37,62 @@ class LinkedList
   end
 
   def first
+    head.next
   end
 
   def last
+    tail.prev
   end
 
   def empty?
+    head.next == tail && tail.prev == head
   end
 
   def get(key)
+    node = target(key)
+    node.val if node
   end
 
   def include?(key)
+    !!target(key)
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    node.next = tail
+    node.prev = tail.prev
+    node.prev.next = node 
+    tail.prev = node
   end
 
   def update(key, val)
+    node = target(key)
+    node.val = val if node
   end
 
   def remove(key)
+    target(key).remove
   end
 
   def each
+    node = head.next
+    while node.next
+      yield node
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
   # def to_s
   #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   # end
+
+  private
+  attr_reader :head, :tail
+
+  def target(key)
+    self.find do |node|
+      node.key == key
+    end
+  end
 end
