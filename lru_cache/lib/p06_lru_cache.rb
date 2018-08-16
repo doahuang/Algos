e4e6672c8716ed17fde5ef1c29407e0ae4115341
doahuang <doahuang@gmail.com>
2018-08-16 00:01:58 -0700

    Passed lru cache

--- lru_cache/lib/p06_lru_cache.rb
+++ lru_cache/lib/p06_lru_cache.rb
@@ -15,6 +15,12 @@ class LRUCache
   end
 
   def get(key)
+    node = map[key]
+    if node
+      update_node!(node)
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -22,15 +28,26 @@ class LRUCache
   end
 
   private
+  attr_reader :store, :map, :max, :prc
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = prc.call(key)
+    node = store.append(key, val)
+    map[key] = node
+    eject! if count > max
+    val
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node.remove
+    map[node.key] = store.append(node.key, node.val)
   end
 
   def eject!
+    node = store.first
+    map.delete(node.key)
+    node.remove
   end
 end
