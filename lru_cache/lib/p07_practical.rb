80de9d9ca5c71926fb441267fa0166384f2f9a5b
doahuang <doahuang@gmail.com>
2018-08-16 00:02:58 -0700

    Passed form palindrome

--- lru_cache/lib/p07_practical.rb
+++ lru_cache/lib/p07_practical.rb
@@ -1,5 +1,17 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
+def can_string_be_palindrome?(string)
+  map = HashMap.new
   
+  string.chars.each do |char|
+    map[char] ? map[char] += 1 : map[char] = 1
+  end
+  
+  odd_chars = 0
+
+  map.each do |char, count|
+    odd_chars += 1 if count.odd?
+  end
+
+  odd_chars < 2
 end
