80a7bf6c4ee5e475997cf4d1458474046b7c612f
doahuang <doahuang@gmail.com>
2018-09-06 23:12:37 -0700

    Passed k largest elements

--- heap_sort/lib/k_largest_elements.rb
+++ heap_sort/lib/k_largest_elements.rb
@@ -1,5 +1,16 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-  
+  heap = BinaryMinHeap.new
+
+  k.times do |i|
+    heap.push(array.pop)
+  end
+
+  until array == []
+    heap.push(array.pop)
+    heap.extract
+  end
+
+  heap.store
 end
