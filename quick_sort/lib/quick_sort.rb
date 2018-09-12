aa811850c0fe6b0ed0853d4605844724ea4355f3
doahuang <doahuang@gmail.com>
2018-09-11 18:53:52 -0700

    Added not in-place quicksort

---
+++ quick_sort/lib/quick_sort.rb
@@ -0,0 +1,24 @@
+class QuickSort
+  # Quick sort has average case time complexity O(nlogn), but worst
+  # case O(n**2).
+
+  # Not in-place. Uses O(n) memory.
+  def self.sort1(array)
+    return array if array.length <= 1
+
+    pivot = array[0]
+    left = array.select{ |el| el < pivot }
+    right = array.select{ |el| el > pivot }
+
+    return sort1(left) + [pivot] + sort1(right)
+  end
+
+  # In-place.
+  def self.sort2!(array, start = 0, length = array.length, &prc)
+    
+  end
+
+  def self.partition(array, start, length, &prc)
+    
+  end
+end
