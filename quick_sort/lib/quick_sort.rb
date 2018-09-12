d7d3dca5f8523c9d88c2a8c7515931e2896a7a39
doahuang <doahuang@gmail.com>
2018-09-11 21:48:48 -0700

    Finished in-place quicksort

--- quick_sort/lib/quick_sort.rb
+++ quick_sort/lib/quick_sort.rb
@@ -15,10 +15,34 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
-    
+    return array if length <= 1
+
+    pivot_idx = partition(array, start, length, &prc)
+
+    left_length = pivot_idx - start
+    right_length = length - left_length - 1
+
+    sort2!(array, start, left_length, &prc)
+    sort2!(array, pivot_idx + 1, right_length, &prc)
+
+    array
   end
 
   def self.partition(array, start, length, &prc)
-    
+    prc ||= Proc.new{ |x, y| x <=> y }
+
+    pivot = array[start]
+    pivot_idx = start
+
+    (start + 1...start + length).each do |i|
+      current = array[i]
+      if prc.call(current, pivot) == -1
+        array[pivot_idx + 1], array[i] = current, array[pivot_idx + 1]
+        pivot_idx += 1
+      end
+    end
+
+    array[pivot_idx], array[start] = pivot, array[pivot_idx]
+    pivot_idx
   end
 end
